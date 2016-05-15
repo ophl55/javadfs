ece63d54849b7b7c1283483bea55542c17804d8d
ophl55 <oliver.philipps@mni.thm.de>
2016-05-15 03:33:59 +0200

    adds cache for read and client-site filepointer

--- src/DATSI/SD/JAVADFS.2015/dfs/DFSCliente.java
+++ src/DATSI/SD/JAVADFS.2015/dfs/DFSCliente.java
@@ -6,21 +6,33 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * DFSCliente: encapsula acceso a rmiregistry y a variables entorno
  * Host y puerto del rmiregistry en variables de entorno
  *
- * TODO: Acceso a las variables de entorno y operación de lookup en el rmiregistry.
+ * TODO Fase 1:
+ * Acceso a las variables de entorno y operación de lookup en el rmiregistry.
+ *
+ * TODO Fase 2 Etapa 1:
+ * mantenga algún tipo de contenedor para almacenar las cachés de todos los cacheFicheros accedidos por la aplicación
+ * hasta ese instante. Cada vez que se cree un objeto de la clase DFSFicheroCliente,
+ * éste deberá comprobar si ya hay una caché para ese fichero. Si la hay, la utilizará. En caso contrario,
+ * la creará y la incluirá en el contenedor para sucesivos accesos.
+ *
  */
 public class DFSCliente {
     private int tamBloque, tamCache;
     private DFSServicio srv;
+    private Map<String, Cache> cacheFicheros;
 
     public DFSCliente(int tamBloque, int tamCache) {
 
         this.tamBloque = tamBloque;
         this.tamCache = tamCache;
+        this.cacheFicheros = new HashMap<String, Cache>();
 
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
@@ -40,6 +52,30 @@ public class DFSCliente {
 
     public void setSrv(DFSServicio srv) {
         this.srv = srv;
+    }
+
+    public Map<String, Cache> getCacheFicheros() {
+        return cacheFicheros;
+    }
+
+    public void setCacheFicheros(Map<String, Cache> cacheFicheros) {
+        this.cacheFicheros = cacheFicheros;
+    }
+
+    public int getTamBloque() {
+        return tamBloque;
+    }
+
+    public void setTamBloque(int tamBloque) {
+        this.tamBloque = tamBloque;
+    }
+
+    public int getTamCache() {
+        return tamCache;
+    }
+
+    public void setTamCache(int tamCache) {
+        this.tamCache = tamCache;
     }
 }
 
--- src/DATSI/SD/JAVADFS.2015/dfs/DFSFicheroCliente.java
+++ src/DATSI/SD/JAVADFS.2015/dfs/DFSFicheroCliente.java
@@ -3,6 +3,7 @@
 package dfs;
 
 import java.io.*;
+import java.net.Authenticator;
 import java.nio.ByteOrder;
 import java.rmi.*;
 
@@ -16,16 +17,34 @@ import java.rmi.*;
  * explicado en el enunciado de la practica.
  */
 public class DFSFicheroCliente  {
-    DFSFicheroServ ficheroServ;
+    private DFSCliente dfs;
+    private DFSFicheroServ ficheroServ;
+    private Cache cache;
+    private long pointer;
 
     public DFSFicheroCliente(DFSCliente dfs, String nom, String modo)
       throws RemoteException, IOException, FileNotFoundException {
-        System.out.println("Create new file " + nom);
-        ficheroServ = dfs.getSrv().iniciar(nom, modo);
+        this.dfs = dfs;
+        this.ficheroServ = dfs.getSrv().iniciar(nom, modo);
+        this.pointer = 0;
+
+        if (dfs.getCacheFicheros().containsKey(nom)){
+            cache = dfs.getCacheFicheros().get(nom);
+        } else {
+            cache = new Cache(dfs.getTamCache()/dfs.getTamBloque());
+            dfs.getCacheFicheros().put(nom, cache);
+        }
     }
 
     /**
      * reads b.length bytes from remote file.
+     *
+     * TODO Fase 2 Etapa 1:
+     * En cuanto a la operación de lectura, debe descomponer el acceso en bloques y por cada bloque comprobar si está
+     * en la caché. En caso negativo, lo solicita al servidor y, una vez obtenido, lo incluye en la caché
+     * (nótese que no hace falta comprobar si esa inclusión expulsa algún bloque puesto que asumimos en esta etapa que
+     * la caché no se llena). Una vez el bloque en la caché, ya sea porque estaba inicialmente o porque se ha traído
+     * del servidor, se copia al buffer especificado por la aplicación.
      *
      * @param b Buffer to be filled by this function
      * @return number of bytes read
@@ -33,10 +52,34 @@ public class DFSFicheroCliente  {
      * @throws IOException
      */
     public int read(byte[] b) throws RemoteException, IOException {
-        byte [] res = new byte[b.length];
-        res = ficheroServ.read(b);
-        System.arraycopy(res, 0, b, 0, b.length);
-        return res.length;
+
+        for (int i = 0; i < b.length/dfs.getTamBloque(); i++){
+            if(cache.getBloque(pointer) != null){
+                // Block found in cache. Now copy it to buffer.
+                System.out.println("Found block in cache");
+                System.arraycopy(cache.getBloque(pointer).obtenerContenido(), 0, b, i*dfs.getTamBloque(), dfs.getTamBloque());
+            }
+            else {
+                // Block not found in cache. Look it up in the remote file.
+                System.out.println("Request block from server");
+                byte [] readBlock = new byte[dfs.getTamBloque()];
+                ficheroServ.seek(pointer);  // Remote pointer has to be adjusted.
+                readBlock = ficheroServ.read(readBlock);
+                if (readBlock == null)
+                    return -1;
+
+                // Store looked up block in the cache.
+                cache.putBloque(new Bloque(pointer, readBlock));
+
+                // Copy block to the buffer.
+                System.arraycopy(readBlock, 0, b, i*dfs.getTamBloque(), dfs.getTamBloque());
+            }
+            pointer += dfs.getTamBloque();
+        }
+
+        //res = ficheroServ.read(b);
+        //System.arraycopy(res, 0, b, 0, b.length);
+        return b.length;
     }
 
     /**
@@ -56,7 +99,8 @@ public class DFSFicheroCliente  {
      * @throws IOException
      */
     public void seek(long p) throws RemoteException, IOException {
-        ficheroServ.seek(p);
+        //ficheroServ.seek(p);
+        pointer = p;
     }
 
     /**
